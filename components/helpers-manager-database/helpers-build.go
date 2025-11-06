942baf82cac11f03f9f6b7917137a4a1ceff38fc
madrahal202 <madrahal202@users.noreply.github.com>
2025-11-06 09:02:43 +0000

    Add helpers-build.go, layouts-fixture-account.styl

---
+++ components/helpers-manager-database/helpers-build.go
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/crowdsecret_fhe/components/helpers-manager-database/helpers-build.go
+// Generated at: 2025-12-03T23:45:45.399Z
+
